4962f67592293dae205682ba25e386daeddd7efd
simmyshiundu <simmyshiundu@gmail.com>
2025-05-20 12:32:43 +0300

    2025-05-20 (Tuesday)

--- database.py
+++ database.py
@@ -12,27 +12,26 @@ conn = psycopg2.connect(user='postgres', password='3Scubed3',
 cur = conn.cursor()  # cur -> GLOBAL VARIABLE which can be used outside a function can be accessed everywhere around the global environment
 
 # query for displaying data in products table
-cur.execute('select * from products;')
+# cur.execute('select * from products;')
 
-print('list of tuples in products table')
+# print('list of tuples in products table')
 
-products = cur.fetchall()
-for p in products:
-    print(p)
+# products = cur.fetchall()
+# for p in products:
+#     print(p)
 
 # query for displaying data in sales table
-cur.execute('select * from sales;')
+# cur.execute('select * from sales;')
 
-print('')
-print('list of tuples in sales table')
+# print('')
+# print('list of tuples in sales table')
 
-sales = cur.fetchall()
-for s in sales:
-    print(s)
+# sales = cur.fetchall()
+# for s in sales:
+#     print(s)
 
 # FETCH ALL PRODUCTS TABLE FUNCTION
-def fetchProducts():
-
+def fetchProducts() :
     cur.execute('select * from products;')
     products = cur.fetchall()
     return products
@@ -41,8 +40,7 @@ def fetchProducts():
         # print(p)
     
 # FETCH ALL SALES TABLE FUNCTION
-def fetchSales():
-
+def fetchSales() :
     # cur -> LOCAL VARIABLE which is used inside a function, hence 'cur' VARIABLE that exists outside a function can be used by a function, it doesn't have to exist inside a function.
     cur.execute('select * from sales;')
     sales = cur.fetchall()
@@ -51,44 +49,44 @@ def fetchSales():
         # return s -> NOT APPLICABLE
         # print(s)
     
-print('')
-print('list of tuples in products table using fetchProducts() function')
-fetchProducts()
+# print('')
+# print('list of tuples in products table using fetchProducts() function')
+# fetchProducts()
 
-print('')
-print('list of tuples in sales table using fetchSales() function')
-fetchSales()
+# print('')
+# print('list of tuples in sales table using fetchSales() function')
+# fetchSales()
 
 
 # DAY 68 2025-04-17 (Thursday)
 
-cur.execute("insert into products(name,buying_price,selling_price,stock_quantity)values('juice',100.99,150.59,70)")
+# cur.execute("insert into products(name,buying_price,selling_price,stock_quantity)values('juice',100.99,150.59,70)")
 
 # from datetime import datetime
 now = datetime.now()
-cur.execute(f"insert into sales(pid,quantity,created_at)values(2,55.00,'{now}')")
-conn.commit()
+# cur.execute(f"insert into sales(pid,quantity,created_at)values(2,55.00,'{now}')")
+# conn.commit()
 
 # INSERT TO PRODUCTS TABLE FUNCTION
-def insert_products() :
-    cur.execute("insert into products(name,buying_price,selling_price,stock_quantity)values('juice',100.99,150.59,70)")
-    conn.commit()
+# def insert_products() :
+#     cur.execute("insert into products(name,buying_price,selling_price,stock_quantity)values('juice',100.99,150.59,70)")
+#     conn.commit()
 
 # INSERT TO SALES TABLE FUNCTION
-def insert_sales() :
-    cur.execute(f"insert into sales(pid,quantity,created_at)values(2,55.00,'{now}')")
-    conn.commit()
+# def insert_sales() :
+#     cur.execute(f"insert into sales(pid,quantity,created_at)values(2,55.00,'{now}')")
+#     conn.commit()
 
 # insert sales - method 1 - takes values as parameters
 def insert_sales2(values) :
-    insert = "insert into sales(pid,quantity,created_at)values(%s,%s,'now()')"
+    insert = "insert into sales(pid,quantity,created_at)values(%s,%s,'now()');"
     cur.execute(insert,values)
     conn.commit()
 
 # insert sales - method 2 - still takes values as parameters but doesn't use placeholders.
 # instead we replace placeholders with {values} parameters in a formatted string.
 def insert_sales3(values) :
-    insert = f"insert into sales(pid,quantity,created_at)values({values})"
+    insert = f"insert into sales(pid,quantity,created_at)values({values});"
     cur.execute(insert)
     conn.commit()
 
@@ -96,7 +94,6 @@ def insert_sales3(values) :
 
 # functions assists in reusability/modularity/easier debugging/dynamic
 # databases have to have atomicity/integrity
-
 
 # TASK: Modify your select and insert functions to be able to select and insert data from any table.
 # HINT: Let your function take parameters (table). 
@@ -106,43 +103,43 @@ def fetch_data(table) :
     data = cur.fetchall()
     return data
 
-products = fetch_data('products')
-sales = fetch_data('sales')
+# products = fetch_data('products')
+# sales = fetch_data('sales')
 
-print("")
-print("Products from fetch data function:\n",products)
-print("")
-print("Sales from fetch data function:\n",sales)
+# print("")
+# print("Products from fetch data function:\n",products)
+# print("")
+# print("Sales from fetch data function:\n",sales)
 
 
 # insert products - method 1 - takes values as parameters
 def insert_products2(values) :
     # use placeholders
-    insert = "insert into products(name,buying_price,selling_price,stock_quantity)values(%s,%s,%s,%s)"
+    insert = "insert into products(name,buying_price,selling_price)values(%s,%s,%s);"
     cur.execute(insert,values)
     conn.commit() #saving operations
 
-product_values = ("Apple",120,150,200)
-product_values1 = ("Banana",130,160,240)
-insert_products2(product_values)
-insert_products2(product_values1)
-products = fetch_data('products')
-print("")
-print("Fetching data after modifying function:\n",products)
+# product_values = ("Apple",120,150,200)
+# product_values1 = ("Banana",130,160,240)
+# insert_products2(product_values)
+# insert_products2(product_values1)
+# products = fetch_data('products')
+# print("")
+# print("Fetching data after modifying function:\n",products)
 
 
 # insert products - method 2 - still takes values as parameters but doesn't use placeholders.
 # instead we replace placeholders with {values} parameters in a formatted string.
 def insert_products3(values) :
-    insert = f"insert into products(name,buying_price,selling_price,stock_quantity)values{values}"
+    insert = f"insert into products(name,buying_price,selling_price)values{values};"
     cur.execute(insert)
     conn.commit() # save data operations
 
-product_values2 = ("Mango",40,50,100)
-insert_products3(product_values2)
-products = fetch_data('products')
-print("")
-print("Fetching products - Method 2:\n",products)
+# product_values2 = ("Mango",40,50,100)
+# insert_products3(product_values2)
+# products = fetch_data('products')
+# print("")
+# print("Fetching products - Method 2:\n",products)
 
 
 # method 3 - insert data into multiple tables with varying number of columns
@@ -151,23 +148,23 @@ def insert_data(table,columns,values) :
     cur.execute(f"insert into {table}({columns})values{values}") # f => formatted string
     conn.commit()
 
-table = 'products'
-columns = "name,buying_price,selling_price,stock_quantity"
-values = ("Avocado",30,40,50)
+# table = 'products'
+# p_columns = "name,buying_price,selling_price,stock_quantity"
+# p_values = ("Avocado",30,40,50)
 
-table_2 = 'sales'
-sales_columns = "pid,quantity,created_at"
-sales_values = (2,55.00,"now()")
+# table_2 = 'sales'
+# s_columns = "pid,quantity,created_at"
+# s_values = (2,55.00,"now()")
 
-insert_data(table,columns,values)
-products = fetch_data('products')
-print("")
-print("Products data from the last method:\n",products)
+# insert_data(table,p_columns,p_values)
+# products = fetch_data('products')
+# print("")
+# print("Products data from the last method:\n",products)
 
-insert_data(table_2,sales_columns,sales_values)
-products = fetch_data('sales')
-print("")
-print("Sales data from the last method:\n",sales)
+# insert_data(table_2,s_columns,s_values)
+# products = fetch_data('sales')
+# print("")
+# print("Sales data from the last method:\n",sales)
 
 # DAY 75 2025-04-30 (Wednesday)
 
@@ -228,3 +225,52 @@ def add_user(user_details) :
     conn.commit() # commit changes made to the database
     cur.close() # closes database session
 
+# fetch stock
+def fetchStock() :
+    cur.execute('select * from stock;')
+    stock = cur.fetchall()
+    return stock
+
+# get stock
+def get_stock(pid) :
+    query = "select stock_quantity from stock where pid = %s;"
+    cur.execute(query,(pid,))
+    stock = cur.fetchone()
+    return stock
+    # conn.commit()
+
+# add stock - method 1 - takes values as parameters
+def insert_stock(values) :
+    insert = "insert into stock(pid,stock_quantity,created_at)values(%s,%s,'now()');"
+    cur.execute(insert,values)
+    conn.commit()
+
+# get contacts
+# def fetchContacts() :
+#     cur.execute('select * from contacts;')
+#     contacts = cur.fetchall()
+#     return contacts
+
+# add contact - method 1 - takes values as parameters
+# def add_contact(values) :
+#     insert = "insert into contacts(full_name,email_address,phone_number,message)values(%s,%s,%s,%s);"
+#     cur.execute(insert,values)
+#     conn.commit()
+
+# coalesce can return zero(0) which is not NULL/empty value 
+def available_stock(pid) :
+    cur.execute("select coalesce(sum(stock.stock_quantity), 0) from stock where pid = %s;", (pid,))
+    total_stock = cur.fetchone()[0]
+    cur.execute("select coalesce(sum(sales.quantity), 0) from sales where pid = %s;", (pid,))
+    total_sold = cur.fetchone()[0] or 0
+    return total_stock - total_sold
+
+def product_name(pid) :
+    cur.execute("select name from products where id = %s;", (pid,))
+    product = cur.fetchone()[0] or "Unknown Product"
+    return product
+
+def edit_product(values) :
+    cur.execute("update products set name = %s,buying_price = %s,selling_price = %s where id = %s;", values)
+    conn.commit()
+    
--- main.py
+++ main.py
@@ -2,19 +2,20 @@
 
 # import flask to use it
 # render_template() has to be imported from Flask
-from flask import Flask, render_template, request, redirect, url_for
-from database import fetchProducts, fetchSales, insert_products2, insert_sales2, profit_per_product, profit_per_day, sales_per_product, sales_per_day, check_user, add_user
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from database import fetchProducts, fetchSales, insert_products2, insert_sales2, profit_per_product, profit_per_day, sales_per_product, sales_per_day, check_user, add_user, fetchStock, insert_stock, available_stock, edit_product
 from flask_bcrypt import Bcrypt
+from functools import wraps # decorator function which starts with '@' & influences the behaviour of another function
 
 # instantiate your application - initialization of Flask
 # flask instance
 app = Flask(__name__)
+app.secret_key = 'myduka1' # secures data
 
 # __name__ - special built-in variable that represents the name of the current file where the application is built.
 #         - tells Flask where the project starts
 
 bcrypt = Bcrypt(app)
-
 
 @app.route('/')  # function 1
 def home():  # function 2
@@ -24,8 +25,10 @@ def home():  # function 2
     # user = {"name":"Simmy","location":"Nairobi"}
     # return render_template("index.html",data_dict = user)
 
-    numbers = [1, 2, 3, 4, 5]
-    return render_template("index.html",numbers = numbers)
+    # numbers = [1,2,3,4,5]
+    # return render_template("index.html",numbers = numbers)
+    
+    return render_template("index.html")
 
 # def print_name() :
 #     print("Michael")
@@ -37,62 +40,75 @@ def home():  # function 2
 
 # running the application
 
+def login_required(f) : # login_required() implements decorator function with @wraps()
+    @wraps(f) # decorator function influences the behaviour of another function
+    def protected(*args,**kwargs) : # function to be called everytime a function is executed. kwargs -> key word arguments
+        if 'email' not in session :
+            return redirect(url_for('login'))
+        return f(*args,**kwargs)
+    return protected
 
 @app.route('/products')
-def products():
+@login_required
+def products() :
     products = fetchProducts()
-    return render_template("products.html",products = products)
-
+    return render_template("products2.html",products = products)
 
 @app.route('/add_products',methods = ['GET', 'POST'])
-def add_products():
+def add_products() :
     if request.method == 'POST':
         product_name = request.form['p-name']
         buying_price = request.form['b-price']
         selling_price = request.form['s-price']
-        stock_quantity = request.form['quantity']
         new_product = (product_name, buying_price,
-                       selling_price, stock_quantity)
+                       selling_price)
         insert_products2(new_product)
         return redirect(url_for('products'))
-
+    
+@app.route('/update_product',methods = ['GET','POST'])
+def update_product() : # same as edit_product() function in database.py
+    if request.method == 'POST' :
+        product_name = request.form['p-name']
+        buying_price = request.form['b-price']
+        selling_price = request.form['s-price']
+        product_id = request.form['p-id']
+        edited_product = (product_name,buying_price,selling_price,product_id)
+        edit_product(edited_product)
+        flash("Product edited successfully","success")
+        return redirect(url_for('products'))
 
 @app.route('/sales')
-def sales():
+def sales() :
     products = fetchProducts()
     sales = fetchSales()
-    return render_template("sales.html", sales=sales, products=products)
-
+    return render_template("sales.html",sales = sales,products = products)
 
 @app.route('/make_sale',methods = ['POST'])
-def make_sale():
+def make_sale() :
     # if request.method == 'POST' :
     product_id = request.form['p-id']
     quantity = request.form['quantity']
     new_sale = (product_id, quantity)
+    # created_at = request.form['created-at'] created_at NOT INCLUDED => self-generated because of 'now()' function
+    stock_available = available_stock(product_id)
+
+    if stock_available < float(quantity) :
+        flash("Insufficient stock","info")
+
     insert_sales2(new_sale)
+    flash("Sale made successfully","success")
     return redirect(url_for('sales'))
 
-# @app.route('/make_sales',methods = ['GET','POST'])
-# def make_sales() :
-#     if request.method == 'POST' :
-#         product_id = request.form['p-id']
-#         quantity = request.form['quantity']
-#         created_at = request.form['created-at'] # created_at NOT INCLUDED => self-generated because of 'now()' function
-#         new_sales = (product_id,quantity,created_at)
-#         insert_sales2(new_sales)
-#         return redirect(url_for('sales'))
-
-
 @app.route('/dashboard')
-def dashboard():
+@login_required
+def dashboard() :
     profit_p_product = profit_per_product()
     sales_p_product = sales_per_product()
 
     profit_p_day = profit_per_day()
     sales_p_day = sales_per_day()
 
-    # type conversion / type casting
+    # type conversion / type casting - changing data from one datatype to another. NB: data is usually sent in string form.
     # list comprehension => RESEARCH:
     product_name = [i[0] for i in profit_p_product]
     p_profit = [float(i[1]) for i in profit_p_product]
@@ -106,27 +122,40 @@ def dashboard():
 
     # return render_template("dashboard.html",profit_p_product = profit_p_product,profit_p_day = profit_p_day,sales_p_product = sales_p_product,sales_p_day = sales_p_day)
 
-    return render_template("dashboard.html", product_name=product_name, p_profit=p_profit, p_sales=p_sales, date=date, p_day=p_day, s_day=s_day)
-
+    return render_template("dashboard.html",product_name = product_name,p_profit = p_profit,p_sales = p_sales,date = date,p_day = p_day,s_day = s_day)
 
 @app.route('/orders')
-def orders():
-    return render_template("orders.html",)
-
+def orders() :
+    return render_template("orders.html")
 
 @app.route('/items')
-def items():
-    items = ['chair', 'desk', 'table']
-    return render_template("items.html", items=items)
-
+def items() :
+    items = ['chair','desk','table']
+    return render_template("items.html",items = items)
 
 @app.route('/sample')
-def sample():
+def sample() :
     return render_template("sample.html")
 
+@app.route('/contact-us')
+def contact_us() :
+    # contacts = fetchContacts()
+    return render_template("contact_us.html")
 
-@app.route('/register', methods=['GET','POST'])
-def register():
+@app.route('/add_contact',methods = ['GET','POST'])
+def add_contact() :
+    if request.method == 'POST' :
+        full_name = request.form['full-name']
+        email_address = request.form['email-address']
+        phone_number = request.form['phone-number']
+        message = request.form['message']
+        new_contact = (full_name, email_address, phone_number, message)
+        add_contact(new_contact)
+        flash("Contact added","success")
+        return redirect(url_for('contact-us'))
+
+@app.route('/register',methods = ['GET','POST'])
+def register() :
     if request.method == 'POST' :
         name = request.form['full-name']
         phone_number = request.form['phone-number']
@@ -144,7 +173,6 @@ def register():
 
     return render_template("register.html")
 
-
 @app.route('/login',methods = ['GET','POST'])
 def login() :
     if request.method == 'POST' :
@@ -153,18 +181,60 @@ def login() :
 
         user = check_user(email)
         if not user :
+            flash("User not found, please register","danger")
             return redirect(url_for('register'))
         else :
             if bcrypt.check_password_hash(user[-1],password) :
+                flash("Logged in","success")
+                session['email'] = email
                 return redirect(url_for('dashboard'))
             else :
+                flash("Passwords don't match","danger")
                 print('Wrong password')
                 
     return render_template("login.html")
 
+# DAY 80 2025-05-07 (Thursday)
+
+# login_required() function MUST BE defined ABOVE decorator functions
+
+# def login_required(f) : # login_required() implements decorator function with @wraps()
+#     @wraps(f) # decorator function influences the behaviour of another function
+#     def protected() : # function to be called everytime a function is executed
+#         if 'email' not in session :
+#             return redirect(url_for('login'))
+#         return protected
+
+# DAY 83 2025-05-13 (Tuesday)
+
+# protecting navbar route
+@app.route('/logout') # session.clear => session.pop
+@login_required
+def logout() :
+    session.pop('email',None)
+    flash("You have been logged out","info")
+    return redirect(url_for('login'))
+
+@app.route('/stock')
+def stock() :
+    # get products & get stock
+    products = fetchProducts()
+    stock = fetchStock() # use Jinja to pass values onto HTML page
+    return render_template("stock.html",products = products,stock = stock)
+
+@app.route('/add_stock',methods = ['GET','POST'])
+def add_stock() :
+    if request.method == 'POST' :
+        product_id = request.form['p-id']
+        stock_quantity = request.form['stock-quantity']
+        new_stock = (product_id,stock_quantity)
+        insert_stock(new_stock) # use different add_stock() function name
+        flash("Stock added","success")
+        return redirect(url_for('stock'))
+    # pass
 
 # run your app
-app.run(debug=True)  # watchdog - way to track errors
+app.run(debug = True)  # watchdog - way to track errors
 
 # inheritance => parent class and child class have similar properties
 # encapsulation => combining data and functions
